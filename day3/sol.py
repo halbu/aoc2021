bec8507dbe34dee316b5767bbc362ce82fc669f7
halbu <fstream.c@gmail.com>
2021-12-07 13:14:27 +0000

    Day 3 part 2

--- day3/sol.py
+++ day3/sol.py
@@ -1,5 +1,27 @@
 d = [l.strip() for l in open("./data", "r") if l != "\n"] 
 
+def filter_by_bit_criteria(my_list, pos, most_common):
+  ones = zeroes = desired_bit = 0
+  for e in my_list:
+    if e[pos] == '1':
+      ones += 1
+    else:
+      zeroes += 1
+
+  if (ones >= zeroes):
+    desired_bit = 1
+  if most_common == False:
+    desired_bit = 1 - desired_bit
+
+  return [e for e in my_list if int(e[pos]) == desired_bit]
+
+def find_rating(my_list, most_common):
+  pos = 0
+  while len(my_list) > 1:
+    my_list = filter_by_bit_criteria(my_list, pos, most_common)
+    pos += 1
+  return my_list[0]
+
 # part 1
 bits = [[] for i in range(len(d[0]))]
 gamma = ''
@@ -17,6 +39,10 @@ for i in bits:
   else:
     gamma += '0'
 
-epsilon = ''.join(['1' if e == '0' else '0' for e in list(gamma)])
+epsilon = ''.join(['1' if e == '0' else '0' for e in list(gamma)]) # flip bits to get epsilon
+print(int(gamma, base=2) * int(epsilon, base=2)) # convert binary strs to decimal & multiply
 
-print(int(gamma, base=2) * int(epsilon, base=2))
+# part 2
+o2_rating = find_rating(d, True)
+co2_rating = find_rating(d, False)
+print(int(o2_rating, base=2) * int(co2_rating, base=2))
