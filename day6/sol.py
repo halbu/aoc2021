b188bd5d598797f2c8499807ecd5822c3da4e343
halbu <fstream.c@gmail.com>
2021-12-07 20:13:50 +0000

    Do file IO inside list comprehension to save 2 valuable lines

--- day6/sol.py
+++ day6/sol.py
@@ -1,12 +1,10 @@
-d = [l for l in open("./data", "r") if l != "\n"]
-
 def advance_one_day():
   timers.append(timers.pop(0))
   timers[6] += timers[8]
     
 # part 1
 timers = [0 for x in range(0, 9)]
-for f in [int(x) for x in d[0].split(',')]:
+for f in [int(x) for x in [l for l in open("./data", "r")][0].split(',')]:
   timers[f] += 1
 [advance_one_day() for i in range(80)]  
 print(sum(timers))
