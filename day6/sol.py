2225d8d370b98396611b82bd00209b2b6b68c425
halbu <fstream.c@gmail.com>
2021-12-07 19:51:02 +0000

    Day 6

---
+++ day6/sol.py
@@ -0,0 +1,16 @@
+d = [l for l in open("./data", "r") if l != "\n"]
+
+def advance_one_day():
+  timers.append(timers.pop(0))
+  timers[6] += timers[8]
+    
+# part 1
+timers = [0 for x in range(0, 9)]
+for f in [int(x) for x in d[0].split(',')]:
+  timers[f] += 1
+[advance_one_day() for i in range(80)]  
+print(sum(timers))
+
+# part 2
+[advance_one_day() for i in range(176)]  
+print(sum(timers))
