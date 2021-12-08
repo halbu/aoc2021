426e52e4f21d9d92362a43e3ef6f781da4741800
halbu <fstream.c@gmail.com>
2021-12-08 11:34:20 +0000

    Refactor for length

--- day4/sol.py
+++ day4/sol.py
@@ -1,28 +1,25 @@
 d = [l.strip() for l in open("./data", "r") if l != "\n"] 
 
 # shape data
-grids = []
-numbers = d.pop(0).split(',')
-for i in range(int(len(d) / 5)):
-  grids.append(d[(i*5):(i*5)+5])
+nums = d.pop(0).split(',') # bingo numbers in the order that they are called
+grids = [d[(i * 5):(i * 5) + 5] for i in range(int(len(d) / 5))] # grids as a list of 5x5 char lists
 for g in grids:
-  for i in range(0, 5):
+  for i in range(5):
     g[i] = g[i].split()
 
-def check_win(grid, idx): # checks a given grid for a bingo win if all numbers up to index position idx (inclusive) have been called
-  for f in range(len(grid[0])):
-    if all(i in numbers[0:idx+1] for i in grid[f]) or all(i in numbers[0:idx+1] for i in [grid[x][f] for x in range(0, 5)]):
+def check_win(g, ix): # checks a grid `g` for a win if all numbers up to index `ix` (inclusive) were called
+  for f in range(len(g[0])):
+    if all(i in nums[0:ix + 1] for i in g[f]) or all(i in nums[0:ix + 1] for i in [g[x][f] for x in range(5)]):
       return True
 
-def find_winner(): # returns [index of winning grid, index of final called number]. assumption: there is always a winning grid
-  for i in range(1, len(numbers)):
+def find_winner(): # returns [index of winning grid, index of last called number] or None if no such grid exists
+  for i in range(1, len(nums)):
     for j in range(len(grids)):
       if check_win(grids[j], i):
         return [j, i]
 
-def eval_score(grid, idx):
-  unmarked_sum = sum([int(grid[i][j]) for i in range(0, 5) for j in range(0, 5) if grid[i][j] not in numbers[0:idx+1]])
-  return int(numbers[idx]) * unmarked_sum
+def eval_score(g, ix): # return a grid's score given the grid `g` and the index `ix` of the last called number
+  return sum([int(g[i][j]) for i in range(5) for j in range(5) if g[i][j] not in nums[0:ix + 1]]) * int(nums[ix])
 
 # part 1
 r = find_winner()
@@ -30,9 +27,9 @@ print(eval_score(grids[r[0]], r[1]))
 
 # part 2 
 while True:
-  result = find_winner()      # this is really inefficient as we start looking for
-  if (len(grids) > 1):        # wins from called_number index 0 again after each
-    grids.pop(result[0])      # pop, but perf is not bad enough (yet!) for me to care
+  r = find_winner()       # this is really inefficient as we start looking for
+  if (len(grids) > 1):    # wins from called_number index 0 again after each
+    grids.pop(r[0])       # pop, but perf is not bad enough (yet!) for me to care
   else:
-    print(eval_score(grids[0], result[1]))
+    print(eval_score(grids[0], r[1]))
     break
