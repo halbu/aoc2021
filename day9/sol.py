b781e70179eb073ccdf4453b62b17d6dd17c36fb
halbu <fstream.c@gmail.com>
2021-12-09 12:04:59 +0000

    Shave off a line

--- day9/sol.py
+++ day9/sol.py
@@ -1,9 +1,8 @@
 d = [list(l.strip('\n')) for l in open("./data", "r")]
-dirs = [[0, -1], [1, 0], [0, 1], [-1, 0]]
 
 # part 1
 def lower_n(x, y): # returns True if any neighbouring number is equal or lower
-  for dir in dirs:
+  for dir in [[0, -1], [1, 0], [0, 1], [-1, 0]]:
     if x + dir[0] >= 0 and x + dir[0] < len(d) and y + dir[1] >= 0 and y + dir[1] < len(d[0]):
       if d[x + dir[0]][y + dir[1]] <= d[x][y]:
         return True
