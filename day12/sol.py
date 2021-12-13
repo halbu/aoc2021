a06f0bf38388b3fe80ade25e7bf5128f5692aa9b
halbu <fstream.c@gmail.com>
2021-12-13 18:28:11 +0000

    Day 12 part 2

--- day12/sol.py
+++ day12/sol.py
@@ -1,6 +1,15 @@
 d = [{'S':x[0], 'E':x[1]} for x in [l.strip("\n").split("-") for l in open("./data", "r")]]
 M, q = {}, []
 
+def has_no_revisits(l):
+  small_caves = [v for v in l if v != 'start' and not v[0].isupper()]
+  return len(small_caves) == len(set(small_caves))
+
+def get_valid_neighbours(l):
+  if has_no_revisits(l):
+    return [n for n in M[c[len(c)-1]] if n != 'start']
+  else: return [n for n in M[c[len(c)-1]] if n not in c or n[0].isupper()]
+
 # part 1
 for edge in d: # create a map of all vertices & connecting edges
   if edge['S'] not in M.keys(): M[edge['S']] = []
@@ -15,5 +24,14 @@ while len(q) > 0:
   for v in [n for n in M[c[len(c)-1]] if n not in c or n[0].isupper()]:
     if v != 'end': q.append(c + [v])
     else: count = count + 1
+print(count)
 
+# part 2
+q.append(['start'])
+count = 0
+while len(q) > 0:
+  c = q.pop()
+  for v in get_valid_neighbours(c):
+    if v != 'end': q.append(c + [v])
+    else: count = count + 1
 print(count)
